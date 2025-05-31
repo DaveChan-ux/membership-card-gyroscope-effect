56f62dc52001823e356a5a83c2df5073cf04f119
DaveChan-ux <david.chan@rewardstyle.com>
2025-05-31 12:53:47 -0700

    Initial Commit

---
+++ membership card gyroscope effect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  membership card gyroscope effect
+//
+//  Created by Dave.Chan on 5/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ membership card gyroscope effect/membership_card_gyroscope_effectApp.swift
@@ -0,0 +1,17 @@
+//
+//  membership_card_gyroscope_effectApp.swift
+//  membership card gyroscope effect
+//
+//  Created by Dave.Chan on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct membership_card_gyroscope_effectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
